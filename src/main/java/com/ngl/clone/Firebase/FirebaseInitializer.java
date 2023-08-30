ff232cb23496e41b218d7c2855bd76cab2c9010c
egeulk <egeulker99@gmail.com>
2023-08-31 00:38:08 +0300

    firebase commits

--- src/main/java/com/ngl/clone/Firebase/FirebaseInitializer.java
+++ src/main/java/com/ngl/clone/Firebase/FirebaseInitializer.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 @Service
 public class FirebaseInitializer {
-    @Value("${app.Firebase-config-file}")
+    @Value("${app.firebase-config-file}")
     String FirebaseConfigPath;
 
     @PostConstruct
