63c2521c0c43e7662a329778e064176864fc39fd
egeulk <egeulker99@gmail.com>
2023-08-30 18:54:17 +0300

    revert

--- src/main/java/com/ngl/clone/websocket/WebSocketConfig.java
+++ src/main/java/com/ngl/clone/websocket/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("https://nursimayasor.com:4200", "https://localhost:4200");
+        registry.addEndpoint("/ws").setAllowedOrigins("https://nursimayasor.com", "https://localhost:4200");
     }
 }
